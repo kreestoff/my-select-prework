91681fd5aed1e3b562949c8e1686d2492d28d3ff
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-20 20:09:35 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,6 +2,10 @@ def my_select(collection)
   i = 0
   result = []
   while i < collection.size 
-    if yield(collection[i]) ==
-  collection.yield[i]
+    if yield(collection[i]) == true 
+    result.push(collection[i])
+  end
+  count += 1 
+end
+result
 end
